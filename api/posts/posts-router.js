3a551317649dfd44075aceafed3595612d01af71
James <JamesWhite1911@protonmail.com>
2021-03-09 18:36:50 -0500

    initial commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,20 @@
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+const Post = require('./posts-model')
+
+//endpoints
+// get /
+router.get('/', (req, res) => {})
+// get /:id
+router.get('/:id', (req, res) => {})
+// get /:id/posts
+router.get('/:id/posts', (req, res) => {})
+// post /
+router.post('/', (req, res) => {})
+// delete /:id
+router.delete('/:id', (req, res) => {})
+// put /:id
+router.put('/:id', (req, res) => {})
+
+module.exports = router;
